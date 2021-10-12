24675fc1f7c6178e16d3678f1e50934143261974
Etornam <sunumacbright@gmail.com>
2021-10-12 16:37:48 +0000

    add light and dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/utilities/utils.dart';
 
 import 'views/home_view.dart';
 
@@ -13,12 +14,38 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Todo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      darkTheme: ThemeData.dark(),
+          textTheme: const TextTheme(bodyText1: TextStyle(color: customBlue)),
+          shadowColor: Colors.white38,
+          primaryColor: Colors.white,
+          scaffoldBackgroundColor: const Color.fromRGBO(249, 251, 255, 1),
+          appBarTheme: const AppBarTheme(
+              backgroundColor: Colors.white,
+              elevation: 0,
+              iconTheme: IconThemeData(color: customBlue),
+              titleTextStyle: TextStyle(
+                  color: customBlue, fontSize: 21, fontWeight: FontWeight.w600),
+              actionsIconTheme: IconThemeData(color: customBlue)),
+          bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+            backgroundColor: Color.fromRGBO(229, 232, 249, 1),
+          )),
+      darkTheme: ThemeData.dark().copyWith(
+          textTheme: const TextTheme(bodyText1: TextStyle(color: Colors.white)),
+          shadowColor: Colors.grey,
+          primaryColor: Colors.black,
+          appBarTheme: const AppBarTheme(
+              elevation: 0,
+              titleTextStyle: TextStyle(
+                  color: Colors.white,
+                  fontSize: 21,
+                  fontWeight: FontWeight.w600),
+              actionsIconTheme: IconThemeData(color: Colors.white)),
+          bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+            backgroundColor: Color.fromRGBO(229, 232, 249, .5),
+          ),
+          floatingActionButtonTheme: const FloatingActionButtonThemeData(
+              backgroundColor: Colors.grey)),
       themeMode: ThemeMode.system,
       home: const HomeView(),
     );
   }
 }
-
