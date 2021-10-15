a3360e6457420acd671fc2fe68b9175f7d8c3422
Etornam <sunumacbright@gmail.com>
2021-10-15 12:24:38 +0000

    change textbutton to row

--- lib/shared_widgets/todo_tile_widget.dart
+++ lib/shared_widgets/todo_tile_widget.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app/models/todo.dart';
 import 'package:todo_app/utilities/utils.dart';
-
+import 'package:todo_app/views/todo_detail_view.dart';
 
 class TodoTileWidget extends StatelessWidget {
   const TodoTileWidget({
@@ -13,41 +13,56 @@ class TodoTileWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      shadowColor: Theme.of(context).shadowColor,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 15),
-        child: ListTile(
-          leading: Icon(
-            todo.status ? Icons.check_circle : Icons.check_circle_outline,
-            size: 30,
-            color: dateColor('Yesterday'),
-          ),
-          title: Text(
-            todo.title,
-            style:
-                Theme.of(context).textTheme.bodyText1!.copyWith(fontSize: 18),
-          ),
-          subtitle: Text(
-            todo.description,
-            style: Theme.of(context)
-                .textTheme
-                .bodyText2!
-                .copyWith(color: Colors.grey),
-          ),
-          trailing: TextButton.icon(
-              onPressed: null,
-              icon: Icon(
-                Icons.notifications,
+    return InkWell(
+      onTap: () {
+        Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+          return TodoDetailView(todo: todo,);
+        },));
+      },
+      child: Card(
+        shadowColor: Theme.of(context).shadowColor,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 15),
+          child: ListTile(
+              leading: Icon(
+                todo.status ? Icons.check_circle : Icons.check_circle_outline,
+                size: 30,
                 color: dateColor(todo.dateTime),
               ),
-              label: Text(
-                todo.dateTime,
+              title: Text(
+                todo.title,
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyText1!
+                    .copyWith(fontSize: 18),
+              ),
+              subtitle: Text(
+                todo.description,
+                maxLines: 3,
+                overflow: TextOverflow.ellipsis,
                 style: Theme.of(context)
                     .textTheme
                     .bodyText2!
-                    .copyWith(color: dateColor(todo.dateTime)),
+                    .copyWith(color: Colors.grey),
+              ),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(
+                    Icons.notifications,
+                    color: dateColor(todo.dateTime),
+                  ),
+                  Text(
+                    todo.dateTime,
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyText2!
+                        .copyWith(color: dateColor(todo.dateTime)),
+                  )
+                ],
               )),
         ),
       ),
